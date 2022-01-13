8ac652629699395b8aac2641d8a02ab67a2459a8
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-01-13 17:06:01 +0530

    Time: 369 ms (15.30%), Space: 117.2 MB (43.63%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,63 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    vector<int> nodesBetweenCriticalPoints(ListNode* head) 
-    {
-        vector<int> ans(2);
-        ans[0]=-1;
-        ans[1]=-1;
-        if(head->next->next==NULL) return ans;
-        ListNode * p=head;
-        ListNode * q=head;
-       p= p->next;
-        int cnt=2;
-        vector<int> v;
-        
-        while(p->next)
-        {
-            if(p->next->val<p->val and p->val >q->val)
-            {
-                v.push_back(cnt);
-                
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        ListNode *prev=head;
+        head=head->next;
+        int i=1;
+        vector<int> index;
+        while(head->next){
+            if((prev->val < head->val and head->val > head->next->val) ||( prev->val > head->val and head->val < head->next->val)){
+                index.push_back(i);
             }
-            
-           if(p->next->val>p->val and p->val <q->val)
-            {
-                v.push_back(cnt);
-                
-                
-            }
-            
-            p=p->next;
-            cnt++;
-            q=q->next;
-            
+            prev=head;
+            head=head->next;
+            i++;
         }
+        if(index.size() < 2) return {-1,-1};
         
-        if(v.size()==0)return ans;
-        if(v.size()==1)return ans;
-        
-        int minDistance=INT_MAX;
-        
-        for(int i=0;i<v.size()-1;i++)
-        {
-           minDistance = min(minDistance,abs(v[i]-v[i+1]));
+        int mindist=INT_MAX;
+        for(int i=0;i<index.size()-1;i++){
+            mindist=min(index[i+1]-index[i],mindist);
         }
-                              
-        int maxDistance=abs(v[0]-v[v.size()-1]);
-         
-        ans[0] = minDistance;              
-        ans[1]=maxDistance;
-        return ans;   
-        
+        return {mindist,index.back()-index[0]};
     }
 };
