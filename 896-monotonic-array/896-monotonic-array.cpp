b1b21ad26ad8f5d767d943fdfa12d951d8ec5946
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-01-22 22:23:44 +0530

    Time: 148 ms (87.73%), Space: 96.5 MB (63.93%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,95 +1,13 @@
 class Solution {
 public:
-    
-    bool monodec(vector<int>& nums)
-    {
-        for(int i=1;i<nums.size()-1;i++)
-            {
-                if(nums[i]<nums[i+1])
-                {
-                    return false;
-                }
-                
-            }
-        
-        return true;
-        
-    }
-    
-    bool monoinc(vector<int>& nums)
-    {
-        for(int i=0;i<nums.size()-1;i++)
-            {
-                if(nums[i]>nums[i+1])
-                {
-                    return false;
-                }
-                
-            }
-        return true;
-    }
-    
-    bool isMonotonic(vector<int>& nums)
-    {
-        if(nums.size()==1) return true;
-        
-        if(nums[0]>nums[1])
-        {
-            
-            
-           if( monodec(nums)==false)return false;
-            
+    bool isMonotonic(vector<int>& A) {
+        bool increase = true;
+        bool decrease = true;
+        for(int i = 0; i < A.size() - 1; i++) {
+            if(A[i] > A[i+1]) increase = false;
+            if(A[i] < A[i+1]) decrease = false;
+            if(increase == false && decrease == false) return false;
         }
-        
-        if(nums[0]<nums[1])
-        {
-            
-            if( monoinc(nums)==false)return false;
-            
-        }
-        
-        if(nums[0]==nums[1])
-        {
-            
-            
-            for(int i=0;i<nums.size()-1;i++)
-            {
-                if(nums[i]==nums[i+1])continue;
-                
-                if(nums[i]>nums[i+1])
-                {
-                    for(;i<nums.size()-1;i++)
-                    {
-                        if(nums[i]<nums[i+1])
-                        {
-                            return false;
-                        }
-                        
-                    }
-                    
-                    break;
-                    
-                }
-                
-             if(nums[i]<nums[i+1])
-             {
-                 for(;i<nums.size()-1;i++)
-                    {
-                        if(nums[i]>nums[i+1])
-                        {
-                            return false;
-                        }
-                        
-                    }
-                    
-                    break;
-                 
-             }
-                 
-            }
-        }
-        
         return true;
-        
     }
 };
