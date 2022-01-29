66c81970631321ed4fce6f87f93ec2f1cd719137
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-01-29 12:19:09 +0530

    Time: 4 ms (46.33%), Space: 8.8 MB (60.82%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -2,83 +2,24 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) 
     {
-        vector<int> v=digits;
-        
-       
-        
-        if(digits[digits.size()-1]!=9)
-        {
-            v[v.size()-1]+=1;
-            
-        }
-        
-       else
-       {  
-         int cnt=0;
-        
-         for(int i=digits.size()-1;i>=0;i--)
-         {
-             if(digits[i]==9)cnt++;
-             
-             
-             if(digits[i]!=9)break;
-                
-             
-             
-         } 
-        
-        if(cnt==digits.size())
-        {
-            for(int i=digits.size()-1;i>=0;i--)
-            {
-                v[i]=0;
-                
-            }
-            
-            v.insert(v.begin(), 1);
-            
-        }
-        
-        if(cnt<digits.size())
-        {
-            int i=digits.size()-1;
-            
-            while(cnt--)
-            {
-                v[i]=0;
-                
-                i--;
-                
-            }
-            
-            
-            v[i]++;
-            
-            
-        }
-           
-       }
         
         
-//         if(digit.size()<=4)
-//         {
-//             int base=1,sum=0;
-//             for(int i=digits.size()-1;i>=0;i--)
-//             {
-//                  sum=digits[i]*base;
-                 
-//                  base*=2;
-                
-                
-//             }
-            
-            
-            
-            
-            
-//         }
+       int n = digits.size();
+	for (int i = n - 1; i >= 0; --i)
+	{
+		if (digits[i] == 9)
+		{
+			digits[i] = 0;
+		}
+		else
+		{
+			digits[i]++;
+			 return digits;
+		}
+	}
+		digits[0] =1;
+		digits.push_back(0);
         
-        return v;
-        
+        return digits;
     }
 };
