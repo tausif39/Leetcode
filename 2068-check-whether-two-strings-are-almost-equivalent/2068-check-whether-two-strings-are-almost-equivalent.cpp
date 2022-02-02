6cec57144acd6d6528f56cc218ed4d9667d0bef7
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-02-02 19:36:09 +0530

    Time: 9 ms (5.65%), Space: 6.3 MB (56.73%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     bool checkAlmostEquivalent(string s, string t) {
-        int cnt[26] = {};
+        int cnt[26] = {0};
         for (char c : s) cnt[c - 'a']++;
         for (char c : t) cnt[c - 'a']--;
         for (int i = 0; i < 26; ++i) {
