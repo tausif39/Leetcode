c759affb1ceaa9c47b6b69d36a2213f0f88a34a5
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-02-06 22:36:26 +0530

    Time: 4 ms (48.29%), Space: 8.9 MB (35.87%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,44 +1,12 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) 
-    {
-        if(nums.size()==0)return 0;
-        // if(nums.size()==0)return 0;
-        
-//         int cnt=0;
-//         int i=0;
-//         while(i<nums.size()-1)
-//         {
-            
-//             while(nums[i]==val)
-//             {
-//                 for(int j=i;j<nums.size()-1;j++)
-//                 {
-//                     nums[j]=nums[j+1];
-                    
-
-
-//                 }
-//                 cnt++;
-                
-//             }
-            
-//             i++;
-                
-            
-//         }
-        int cnt=0;
-        for(int i=0;i<nums.size();i++)if(nums[i]==val)cnt++;
-        
-        
-        
-        
-        remove(nums.begin(),nums.end(),val);
-        
-        
-        
-        
-        return nums.size()-cnt;
-        
-    }
-};
+class Solution { 
+    public:
+        int removeElement(vector<int>& nums, int val) {
+            int index = 0;
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] != val) {
+                    nums[index++] = nums[i];
+                }
+            }
+            return index;
+        }
+    };
