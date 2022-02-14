306134ffe5d2d566bde30a0d0cde8689c96afb0b
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-02-14 11:31:00 +0530

    Time: 4 ms (49.11%), Space: 7.1 MB (33.93%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+       
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=i+1;j<matrix.size();j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+                 
+
+            }
+            
+        }
+        
+        
+//         for(int j=0;j<matrix[0].size();j++)
+//         {
+//             swap(matrix[0][j],matrix[j][0]);
+            
+//         }
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+            
+        }
+        
+        
+        
+    }
+};
