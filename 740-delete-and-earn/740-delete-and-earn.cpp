12f5e2197a10a52d4548ce66dab625bac2270e48
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-05 23:15:02 +0530

    Time: 4 ms (94.57%), Space: 13.8 MB (24.41%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    const int MAX = 10001;
-    
-    int getMaxPoint(vector<int>& cnt, vector<int>& dp, int idx) {
-        if(idx >= MAX) return 0;
-        if(dp[idx] == -1) {
-            dp[idx] = max(idx * cnt[idx] + getMaxPoint(cnt, dp, idx + 2), getMaxPoint(cnt, dp, idx + 1));
-        }
-        return dp[idx];
-    }
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> cnt(10001, 0), dp(10001);
-        for(int i = 0; i < 10001; ++i) dp[i] = -1;
-        for(int i = 0; i < nums.size(); ++i) cnt[nums[i]]++;
-        return getMaxPoint(cnt, dp, 1);
+        int maxi=*max_element(nums.begin(),nums.end());
+        int dp[20005];
+        
+        vector<int> freq(20005);
+        for(auto it:nums)
+            freq[it]++;
+        dp[1]=freq[1];
+        for(int i=2;i<=maxi;i++)
+        {
+            dp[i]=max(dp[i-1],dp[i-2]+i*freq[i]);
+        }
+        return dp[maxi];
     }
 };
