f3135b34ec3698b715e59ff55789d0519db484d8
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-05 08:51:03 +0530

    Time: 32 ms (77.31%), Space: 20.3 MB (64.48%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -4,12 +4,11 @@ public:
     {
         sort(nums.begin(),nums.end());
         
-        int maxdiff=INT_MIN;
+        
         
         int a=nums[0]*nums[1];
         int b=nums[nums.size()-1]*nums[nums.size()-2];
-        
-        // /int c==abs(b-a);
+  
         
         return abs(b-a);
         
