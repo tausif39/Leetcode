c6f6cff4dc1b007a0fac465266c763efb1c1e3d6
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-10 19:06:51 +0530

    Time: 26 ms (64.80%), Space: 11.6 MB (26.65%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    string removeDuplicates(string s) 
-    {
-        string ans;
-        for(char c:s)
-        {
-            if(c == ans.back())
-                ans.pop_back();
-            else
-                ans.push_back(c);
+   string removeDuplicates(string s) {
+        stack<char> st;
+        st.push(s[0]);
+         
+        for(int i = 1; i < s.size(); i++) { 
+            if(!st.empty() && st.top() == s[i]) {
+                st.pop();
+            } else {
+                st.push(s[i]);
+            }
         }
-        return ans;
         
+        string ans = "";
+        
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
