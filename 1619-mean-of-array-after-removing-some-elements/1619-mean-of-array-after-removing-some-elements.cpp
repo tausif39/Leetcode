ed3fed6302f000211c08e26cd792ecfcf9c83f1e
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-17 09:40:06 +0530

    Time: 4 ms (96.02%), Space: 9.5 MB (67.28%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -7,28 +7,33 @@ public:
         int rem=n/20;
         
         sort(arr.begin(),arr.end());
-        int x=rem;
-        while(x--)
-        {
-            arr.pop_back();
-        }
+//         int x=rem;
+//         while(x--)
+//         {
+//             arr.pop_back();
+//         }
         
-        reverse(arr.begin(),arr.end());
+//         reverse(arr.begin(),arr.end());
         
-        while(rem--)
-        {
-            arr.pop_back();
-        }
+//         while(rem--)
+//         {
+//             arr.pop_back();
+//         }
         
         
         double sum=0;
         
-        for(int i=0;i<arr.size();i++)
+        for(int i=0;i<arr.size()-rem;i++)
         {
            sum+=arr[i] ;
+           
+            if(i<rem)sum-=arr[i];
+            
         }
         
-        return sum/arr.size();
+        int size=arr.size()-2*rem;
+        
+        return sum/size;
         
         
     }
