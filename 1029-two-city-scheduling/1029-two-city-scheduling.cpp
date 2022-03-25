ce0096111d8ae020b4e5ccd0cfc3766b786ad603
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-25 21:02:19 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    static bool comp(const pair<int,pair<int,int>> &p1, const pair<int,pair<int,int>> &p2)
-    {
-        return p1.first < p2.first;
-    }
-    
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size();
-        int res = 0;
+        vector<int> diff;
+        int mincost = 0;
         
-        vector<pair<int,pair<int,int>>> v;
-        
-        for(int i=0;i<n;i++)
+        for(int i = 0; i<costs.size(); ++i)
         {
-            v.push_back({costs[i][0] - costs[i][1],make_pair(costs[i][0], costs[i][1])});
+			// cost to send every person to city A
+            mincost += costs[i][0];
+			// computing difference if person sent to city B
+            diff.push_back(costs[i][1] - costs[i][0]);
+        }
+		
+		// sort the diff vector
+        sort(diff.begin(),diff.end());
+        
+		// picking n persons having least diff
+		// sending them to city B
+        for(int i=0; i<costs.size()/2; ++i)
+        {
+            mincost += diff[i];
         }
         
-        sort(v.begin(), v.end(), comp);
+        return  mincost;
         
-        for(int i=0;i<n/2;i++)
-            res += v[i].second.first;
-        
-        for(int i=n/2;i<n;i++)
-            res += v[i].second.second; 
-        
-        return res;
     }
 };
