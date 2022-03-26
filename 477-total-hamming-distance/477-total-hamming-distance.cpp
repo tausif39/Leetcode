22c0d414ae690fd90d9d684c99788828dbad8819
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-03-26 16:10:12 +0530

    Time: 67 ms (57.96%), Space: 19 MB (55.86%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,27 +1,30 @@
-// taking one bit of all the numbers at once
+// The idea behind is that int will come in 32 bit , so we will run loop i=0 to 32 times
+// and will count number of elements having set bit on ith index.
+
+// Let say for i= 0 , number of elements having set bit is c
+// So. this set bit will generate total hamming distance (nums.size()-c) * c.
+
+// Like if array given {1,2,3,4,5}={ 001 , 010 , 011 , 100 , 101 }
+// so at 0th index, number of elements having set bit at 0th index is 3
+// And 2 elements having unset at 0th index.
+// So total hamming distace for i=0 will be 3 * 2=6
+// Same we can go for i=1,2,3,...31 and add all the distance and return final distance
     
 class Solution {
 public:
-    int totalHammingDistance(vector<int>& nums) 
-    {
-        int ans=0, n=nums.size();
-        // if(n<=1) return 0;
-        for(int i=0; i<32; i++)
+ 
+    int totalHammingDistance(vector<int>& nums) {
+        int res=0;
+        for(int i=0;i<32;i++)
         {
-            int one=0, zero=0;
-            for(int j=0; j<n; j++)
+            int c=0;
+            for(int j=0;j<nums.size();j++)
             {
-                if(nums[j]==0) 
-                {
-                    zero++; 
-                    continue;
-                }
-                int t=nums[j]%2;
-                (t==1)? one++: zero++;
-                nums[j] /= 2;                                 
+                if((1<<i)&nums[j]) c++;  //checking ith bit set or not
             }
-            ans += one*zero;
+            res+=(nums.size()-c)*c;
         }
-        return ans;
+        return res;
+        
     }
 };
