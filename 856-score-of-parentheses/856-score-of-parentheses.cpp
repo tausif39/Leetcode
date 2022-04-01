12edebda1420ac2024db4a7dd2d6124285bcbf20
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-04-01 20:14:08 +0530

    Time: 7 ms (14.02%), Space: 6.1 MB (41.85%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int>st;
+        int score=0;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            
+            if(s[i]=='(')
+            {
+                //if we find open parenthesis, push the current score into the stack
+                st.push(score);
+                score=0;//reset the score to 0
+            }
+            
+            else if(s[i]==')')
+            {
+                //if we find close parenthesis
+                score=st.top()+ max(2* score , 1);
+                st.pop();
+            }
+        }
+        
+        return score;//return score
+    }
+};
