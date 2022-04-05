f6469c650fcbe1bca079fa9558475d2013a3afea
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-04-05 13:58:04 +0530

    Time: 5 ms (20.62%), Space: 6 MB (8.72%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -1,70 +1,39 @@
 class Solution {
 public:
-    int convertTime(string current, string correct) 
-    {
-        int h=0;
-        int m=0;
+    int convertTime(string current, string correct) {
+        int totalCurMin = 0;
+        int totalTarMin = 0;
         
-        h+=current[1]-'0' + 10*(current[0]-'0');
+        int curHour = stoi(current.substr(0, 2));
+        int curMin = stoi(current.substr(3));
         
-        m+=current[4]-'0' + 10*(current[03]-'0');
+        totalCurMin = curHour * 60 + curMin;
         
+        int tarMin = stoi(correct.substr(3));
+        int tarHour = stoi(correct.substr(0,2));
         
+        totalTarMin = tarHour * 60 + tarMin;
         
-         int h1=0;
-        int m1=0;
-        
-        h1+=correct[1]-'0' + 10*(correct[0]-'0');
-        
-        m1+=correct[4]-'0' + 10*(correct[03]-'0');
-        
-        int time=0;
-        
-        time+= 60*h + m;
-        
-        
-        int time1=0;
-        
-        time1+= 60*h1 + m1;
-        
-        
-        int cnt=0;
-        
-        
-        int diff=time1- time;
-        
-        
-        
-        while(diff>=60)
-        {
-            diff-=60;
-            cnt++;
+        int diff = totalTarMin - totalCurMin;
+        int result = 0;
+        while (diff > 0){
+            if (diff >= 60){
+                result += (diff/60);
+                diff = diff % 60;
+            }
+            else if (diff >= 15){
+                result += (diff / 15);;
+                diff = diff % 15;
+            }
+            else if (diff >= 5){
+                result += (diff / 5);;
+                diff = diff % 5;
+            }
+            else{
+                result++;
+                diff--;
+            }
         }
-        
-        while(diff>=15)
-        {
-            diff-=15;
-            cnt++;
-        }
-        
-        while(diff>=5)
-        {
-            diff-=5;
-            cnt++;
-        }
-        
-        while(diff>=1)
-        {
-            diff-=1;
-            cnt++;
-        }
-        
-        
-        
-        
-        
-        
-        
-        return cnt;
+        return result;
     }
 };
