e86cb69457fcb7ea1d2a82864395ea5d0eadfad0
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-04-16 12:14:51 +0530

    Time: 102 ms (88.77%), Space: 54.3 MB (29.50%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
@@ -1,51 +1,13 @@
 class Solution {
 public:
-    int addRungs(vector<int>& rungs, int dist) 
-    {
-        // int step=0;
-        int cnt=0;
-        
-        for(int i=0;i<rungs.size();i++)
-        {
-            if(i==0)
-            {
-                if(rungs[i]>dist)
-                {
-                    if(rungs[i]%dist==0)
-                    {
-                        cnt+=(rungs[i]/dist-1);
-                        
-                    }
-                    
-                    else
-                    {
-                        cnt+=(rungs[i]/dist);
-                    }
-                    
-                }
-            }
-            
-          else if((rungs[i]-rungs[i-1])>dist)
-            {
-                int z=rungs[i]-rungs[i-1];
-              if(z%dist!=0)
-              {
-                cnt+=z/dist;
-                  
-              }
-              
-              else
-              {
-                  cnt+=z/dist-1;
-              }
-                
-
-            }
-            
+    int addRungs(vector<int>& rungs, int dist) {
+        int prev = 0;
+        int ans = 0;
+        for(auto r: rungs){
+            int gap = r-prev;
+            ans += (gap-1)/dist;
+            prev = r;
         }
-        
-        
-        return cnt;
-        
+        return ans;
     }
 };
