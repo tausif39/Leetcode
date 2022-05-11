c6ee081917d8035dc0aaf25dc504daa4617efeb2
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-05-11 22:49:38 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.38%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     
-     int count(int i, int n, string &vowels){
+     int count(int i, int n, string &vowels, vector<vector<int>> &dp){
 
-   		if(n==0) return 1; // if a string of length n is formed
+   	  if(n==0) return 1; // if a string of length n is formed
 
-   		if(i>=5) return 0; // if i exceeds the length of vowels
+   	  if(i>=5) return 0; // if i exceeds the length of vowels
 
-   		int pick, notPick;
-   		// pick i.e. pick this alphabet
-   		pick= count(i, n-1, vowels);
-   		// notPick i.e. skip this alphabet
-   		notPick= count(i+1, n, vowels);
+   	  if(dp[i][n]!=-1) return dp[i][n]; // check if dp already contains the answer
 
-   		return pick + notPick;
+   	  int pick, notPick;
+   	  // pick i.e. pick this alphabet
+   	  pick= count(i, n-1, vowels, dp);
+   	  // notPick i.e. skip this alphabet
+   	  notPick= count(i+1, n, vowels, dp);
+
+   	  return dp[i][n]= pick + notPick; // store the value in dp
    }
 
    int countVowelStrings(int n) {
    	 string vowels= "aeiou"; // all vowels in lexicographically sorted order
-   	 return count(0, n, vowels);
+   	 vector<vector<int>> dp(5, vector<int>(n+1, -1)); // declare dp vector
+   	 return count(0, n, vowels, dp);
+   
+    
    }
-    
-    
 };
