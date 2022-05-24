bce5afc8134126222e97ce0b0d82e32f8e9a917d
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-05-24 17:02:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (37.62%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,64 +1,15 @@
 class Solution {
 public:
-    int minStartValue(vector<int>& nums)
-    {
-        
-        
-        // sort(nums.begin(),nums.end());
-        
-        int mini=*min_element(nums.begin(),nums.end());
-        
-            if(mini>-1)return 1;
-        
-        
-        
-        int sv=1;
-        
-       
     
-        while(sv)
-        {
-            
-        int sum=sv;
-        
-           bool flag=false;
-            // cout<<sv<<" ";
-        for(int i=0;i<nums.size();i++)
-        {
-        
-            
-            // if(sv==5)
-            // {
-            //     cout<<sum<<" ";
-            // }
-            
-            sum= sum+ nums[i];
-            
-            if(sum<1)
-            {
-                cout<<1;
-                flag=true;
-                // if(sv==5)cout<<sum;
-                break;
-                
-            }
-           
-            
-        }
-            
-            if(sum>0)
-            {
-                if(flag==false) return sv;
-               
-            }
-            else
-            {
-                sv++;
-            }
-            
-            
-        }
-        
-      return 1;
-    }
+    int minStartValue(vector<int> & nums) {
+int min;
+for(int i=1;i<nums.size();i++){
+nums[i]+=nums[i-1];
+}
+min=*min_element(nums.begin(),nums.end());
+if(min>0)
+return 1;
+else
+return abs(min)+1;
+}
 };
