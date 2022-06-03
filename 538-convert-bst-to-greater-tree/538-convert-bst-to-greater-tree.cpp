c8587b252071c20e74c0b5d242c19e046db70004
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-06-03 18:24:10 +0530

    Time: 50 ms (27.94%), Space: 33.3 MB (93.10%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -9,70 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+   /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     
-    unordered_map<int,int> mp;
-    
-     void inorde(TreeNode* root){
-        if(root==NULL) return;
-        
-        inorde(root->left);   // left
-        root->val=mp[root->val];       // data
-        inorde(root->right); 
-         // right
-    }
-    
-    
-    
-    void inorder(TreeNode* root, vector<int> &v){
-        if(root==NULL) return;
-        
-        inorder(root->left,v);   // left
-        v.push_back(root->val);       // data
-        inorder(root->right,v);   // right
-    }
-    
-    TreeNode* convertBST(TreeNode* root) 
+    int sum=0;
+    void inorder(TreeNode* root)
     {
-        if(!root)return root;
+        if(root==NULL)
+            return;
+        inorder(root->right);
+        sum+=root->val;
+        root->val=sum;
+        inorder(root->left);
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
         
-        // unordered_map<int,int> mp;
-        
-        vector<int> v;
-        inorder(root,v);
-        
-        int n=v.size();
-        sort(v.begin(),v.end());
-        
-        vector<int> suff(n);
-        
-        suff[n-1]=v[n-1];
-        
-        
-        
-       
-        
-        
-        for(int i=n-2;i<v.size();i--)
-        {
-            
-            suff[i]=suff[i+1]+ v[i];
-            
-        }
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            
-            mp[v[i]]=suff[i];
-            
-        }
-        
-        inorde(root);
-        // for(int i:)cout<<i<<" ";
+        inorder(root);
         
         return root;
-        
     }
+
+   
 };
