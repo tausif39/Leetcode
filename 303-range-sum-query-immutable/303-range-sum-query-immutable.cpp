755e7fe6738a8df698261e2a989eba492fd065bb
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-08-09 15:15:11 +0530

    Time: 59 ms (25.78%), Space: 17.2 MB (7.57%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,60 +1,68 @@
 class NumArray {
 public:
-    int n;
-    vector<int> seg;
     
-    int getSum(int index, int segStart, int segEnd, int queryStart, int queryEnd){ //index - index of current segement, 
-        //segStart and segEnd - range of values this segment has summed up
-        //queryStart queryEnd - query left and right indexs
+    vector<int> segtree;
+    int n;    
+    
+    NumArray(vector<int>& nums) 
+    {
+        n= nums.size() ;
         
-        if(queryStart<=segStart && queryEnd>=segEnd){//if our seg completely overlaps with the query range
-            return seg[index];
-        }
+        segtree.resize(4*nums.size());
+        buildtree(0,0,n-1,nums);
         
-        if(segStart>queryEnd || segEnd<queryStart){// our segment does not overlap with the queryrange
+    }
+    
+    
+    void buildtree(int indx, int low, int high,vector<int>& nums)
+    {
+          if (low == high)
+          {
+            segtree[indx] = nums[low];
+              
+            return;
+          }
+
+      int mid = (low + high) >> 1;
+
+      buildtree(2 * indx + 1, low, mid, nums);
+      buildtree(2 * indx + 2, mid + 1, high, nums);
+
+        segtree[indx] = segtree[2 * indx + 1] + segtree[2 * indx + 2];
+        
+    }
+    
+    
+    int query(int indx, int low, int high, int l, int h)
+    {
+          if (h < low || l > high)
             return 0;
-        }
-        
-        //if it overlaps partially we will break down the segment down again
-        int mid = segStart + (segEnd-segStart)/2;
-        int ans = getSum(index*2+1, segStart, mid, queryStart, queryEnd) + 
-            getSum(index*2+2, mid+1, segEnd, queryStart, queryEnd); //searching in left and right
-        return ans;
-        // return 0;
-        
+
+           if (l <= low && high <= h)
+        return segtree[indx];
+
+          int mid = (low + high) >> 1;
+          int left = query(2 * indx + 1, low, mid, l, h);
+
+          int right = query(2 * indx + 2, mid + 1, high, l, h);
+
+          return left + right;
     }
     
-    int constructSegementTree(vector<int>&nums, int index, int start, int end){
-        if(start == end){ // leaf node
-            seg[index] = nums[start];
-            // cout<<seg[index]<<endl;
-            return seg[index];
-        }
+    int sumRange(int left, int right) 
+    {
         
-        //we will divide the array into two parts and recursively store the total sum in our node
-        
-        int mid = start+(end-start)/2;
-        seg[index] = constructSegementTree(nums, 2*index+1, start, mid); //left sise
-        seg[index] += constructSegementTree(nums, 2*index+2, mid+1, end); //right sise
-        // cout<<seg[index]<<endl;
-        return seg[index]; //return the sum store in this node as this is the sum of the subpart of its parent node.
-    }
-    
-    NumArray(vector<int>& nums) {
-        n = nums.size();
-        
-        int x = ceil(log2(n));//height of the segment tree
-        int size = 2 * (int)(pow(2, x)) - 1; //size of segment tree
-        
-        seg.resize(size, 0);
-        
-        constructSegementTree(nums, 0, 0, n-1);
-        // for(auto i: seg){
-        //     cout<<i<<" ";
-        // }cout<<endl;
-    }
-    
-    int sumRange(int left, int right) {
-        return getSum(0, 0, n-1, left, right);
+        // buildtree(0,0,n-1);
+        return query(0,0,n-1, left, right);
     }
 };
+
+
+
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
