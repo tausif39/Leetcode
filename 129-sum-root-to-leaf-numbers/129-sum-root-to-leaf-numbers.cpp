63d75c3e52f1aaff2e7dba7f93f0d4d1787718fc
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-08-11 16:05:24 +0530

    Time: 4 ms (48.02%), Space: 9.2 MB (83.35%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -1,75 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
     
-    int number_to_ssum(vector<int> &nums)
-    {
-        int long long ans=0;
-        
-        int i=nums.size()-1;
-        int  long long base=1;
-        
-        while(i>=0)
-        {
-            int long long digit=base*nums[i];
-            ans+=digit;
-            
-            base*=10;
-            
-            i--;
-        }
-        
-        // cout<<ans<<" ";
-              
-        return ans;
-        
-    }
-    
-    
-    void dfs(TreeNode* root, int &ans,vector<int> nums )
-    {
+    void find_ans(TreeNode* root,int sum,int &ans){
         if(root==NULL)return ;
-        
-        if(root->left==NULL and root->right==NULL)
-        {
-            nums.push_back(root->val);
-            ans+=number_to_ssum(nums);
-            
-            nums.clear();
+        sum=sum*10+root->val;
+        if(root->left==NULL and root->right==NULL){
+            ans+=sum;
             return ;
         }
-        
-        
-        nums.push_back(root->val);
-        dfs(root->left,ans, nums);
-        dfs(root->right,ans, nums);
-        
-        // nums.pop_back();
-        
-        
+        find_ans(root->left,sum,ans);
+        find_ans(root->right,sum,ans);
     }
-    
-    
-    
-    int sumNumbers(TreeNode* root) 
-    {
+    int sumNumbers(TreeNode* root) {
         int ans=0;
-        
-        vector<int> nums;
-        dfs(root,ans, nums);
-        
-       
+        find_ans(root,0,ans);
         return ans;
     }
 };
