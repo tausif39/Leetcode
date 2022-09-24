2af6551af3922bdfe608b63e531872563654882e
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-09-24 18:18:18 +0530

    Time: 178 ms (41.71%), Space: 6 MB (47.06%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,73 +1,30 @@
 class Solution {
 public:
     
-    string DecimalToBinary(int num)
-    {
-       string str;
-          while(num)
-          {
-              if(num & 1) // 1
-                str+='1';
-              else
-                str+='0';
-              num>>=1; 
-                  // Right Shift by 1 
-
-         }  
-        
-      reverse(str.begin(),str.end());
-      return str;
-        
-    }
-    
-    long long MOD=1e9+7;
-    
-    void help(string &s , int n)
-    {
-        
-        for(int i=1;i<=n;i++)
-        {
-            
-            string z=DecimalToBinary(i);
-            s+=z;
-        }
-        
-    }
+    int mod=1e9+7;
     
     int concatenatedBinary(int n) 
     {
+        int long long ans=0;
         
-            // cout<<(12);
-        string s;
-        
-//         help(s,n);
-        
-        // string ans;
-        
-         help(s,n);
-         
-        // cout<<to_string(12);
-        
-         long long sum=0;
-        
-         long long base=1; 
-        
-        // cout<<s;
-        
-        for(int i=s.size()-1;i>=0;i--)
+        for(int i=1;i<=n;i++)
         {
+            int k=i;
             
-            int digit;
-            if(s[i]=='0')digit=0;
-            else digit=1;
-            sum+=(long long)(base*digit);
-            base*=2;
-            base%=MOD;
+            int steps=0;
+            
+            while(k)
+            {
+                k/=2;
+                steps++;
+            }
+            
+            ans=((ans<<steps) + i)%mod;
+            
+            
         }
         
-            sum=sum%MOD;
-         
-        return sum;
+        return ans%mod;
         
         
     }
