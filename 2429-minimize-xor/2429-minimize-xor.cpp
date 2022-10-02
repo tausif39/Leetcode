c64e34a4083080e01bde691ef6f420a2fbca5e50
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-10-02 11:28:48 +0530

    Time: 3 ms (66.67%), Space: 5.9 MB (83.33%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -0,0 +1,37 @@
+class Solution {
+ public:
+ 
+    int minimizeXor(int num1, int num2) {
+    int cnt2 = __builtin_popcount(num2);
+    
+    
+    // counting set bits
+    
+    int ans = 0;
+    
+    // xor of 1 and 1 is 0 , so minimizing num1 by setting bits in ans
+    for(int i = 31 ; i >= 0 ; i--){
+	    int x = 1<<i;
+        if(num1&x){
+            ans = ans | x ;
+            cnt2--;
+            if(cnt2 == 0) break;
+        }
+    }
+    
+    // number of  bits in ans should be same as num2 so setting in ans , the unset bits of num1
+	
+    if(cnt2 > 0){
+        for(int i = 0 ; i < 32 ; i++){
+		    int x = 1<<i;
+            if( (num1&x) == 0 ){
+                ans =  ans | x ;
+                cnt2--;
+                if(cnt2 == 0) break;
+            }
+        }
+    }
+    
+        return ans;
+     }
+   };
