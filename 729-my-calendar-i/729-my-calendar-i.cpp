dbaa242b1e25eb8f5c5fb51d141489e9c5ac3901
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-10-08 11:30:26 +0530

    Time: 134 ms (77.26%), Space: 37.7 MB (75.83%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,51 +1,11 @@
 class MyCalendar {
+private: vector<pair<int,int>> vp;
 public:
+    MyCalendar() {}
     
-    map<pair <int , int> , bool> mp;
-    
-    MyCalendar() {
-        
-    }
-    
-    bool book(int start, int end)
-    {    
-        
-        for(auto [x,y] :mp)
-        {
-            
-            if(start>=x.first and start<x.second)
-            {
-                return false;
-            }
-            
-            if(end>x.first and end<x.second)
-            {
-                return false;
-            }
-            
-            if(x.first>=start and x.first<end)
-            {
-                    return false;
-            }
-            
-            if(x.second>start and x.second<end)
-            {
-                    return false;
-            }
-            
-      
-        }
-        
-        
-        mp[{start,end}] = true;
-        
+    bool book(int start, int end) {
+        for(auto p: vp) if(max(p.first, start) < min(p.second, end)) return false;
+        vp.push_back({start,end});
         return true;
-        
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
