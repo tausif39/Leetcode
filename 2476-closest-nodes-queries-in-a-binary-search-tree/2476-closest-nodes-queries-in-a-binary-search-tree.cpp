5652291500b87d81f856d54628418d0edbdffcdb
Tausiful Haque <91016348+tausif39@users.noreply.github.com>
2022-11-20 10:20:18 +0530

    Time: 408 ms (88.89%), Space: 134.6 MB (33.33%) - LeetHub

---
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void inorder(TreeNode* root, vector<int> &nums) {
+        if (root == NULL) 
+            return;
+        
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+public:
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+        vector<int> nums;
+        inorder(root, nums);
+        vector<vector<int>> res;
+        
+        for (auto num: queries) {
+            int ind = upper_bound(nums.begin(), nums.end(), num) - nums.begin();
+            int ind2 = lower_bound(nums.begin(), nums.end(), num) - nums.begin();
+            
+            int leftAns = -1, rightAns = -1;
+            
+            if (ind - 1 >= 0) leftAns = nums[ind -1];
+            if (ind2 < nums.size()) rightAns = nums[ind2];
+            
+            res.push_back({leftAns, rightAns});
+        }
+        
+        return res;
+    }
+};
