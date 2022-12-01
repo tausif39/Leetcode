09de43b3c45b194c42a53b337e856cbb6051756c
Tausif <91016348+tausif39@users.noreply.github.com>
2022-12-01 10:36:33 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (56.35%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    bool vowels(char c)
+    {
+        return c=='a' || c=='e'  || c=='i'  || c=='o'  || c=='u' || c=='A' || c=='E'  || c=='I'  || c=='O'  || c=='U' ;
+    }
+    
+    bool halvesAreAlike(string s)
+    {
+        int cnt_a=0, cnt_b=0;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(vowels(s[i]) and i<s.size()/2)cnt_a++;
+            else if(vowels(s[i]) and i>=s.size()/2)cnt_b++;
+        }
+        
+        return cnt_a==cnt_b;
+        
+    }
+};
