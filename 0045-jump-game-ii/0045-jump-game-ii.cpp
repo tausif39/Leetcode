ff178bfbe4986a02171d7b808e4d5e54fbd46d1a
Tausif <91016348+tausif39@users.noreply.github.com>
2023-01-01 11:06:48 +0530

    Time: 10 ms (98.18%), Space: 16.5 MB (97.40%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+     int pos=0;
+     int des=0;
+     int jump=0;
+     for(int i=0;i<nums.size()-1;i++)
+     {
+         des=max(des,nums[i]+i);
+         if(pos==i)
+         {
+         pos=des;
+         jump++;
+         }     
+     }
+    return jump;
+    }
+};
