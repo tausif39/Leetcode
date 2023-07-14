a0e7f6607983ea443791955e43f7280952306db6
Tausif <91016348+tausif39@users.noreply.github.com>
2023-07-14 11:17:18 +0530

    Time: 160 ms (16.87%), Space: 61.8 MB (74.55%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-
-    vector<int>dp;
-
-    int func(vector<int>&nums,int target,int i)
+    
+    int maximumJumps(vector<int>& nums, int target) 
     {
-        if(i==nums.size()-1)return 0;
-
-        if(dp[i]!=-1)return dp[i];
-        int a=INT_MIN;
-        for(int j=i+1;j<nums.size();j++)
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        dp[0]=0;
+        
+        for(int i=1;i<n;i++)
         {
-            if(abs(nums[i]-nums[j])<=target)
+            
+            for(int j=0;j<i;j++)
             {
-                a=max(a,func(nums,target,j)+1);
+                if(abs(nums[i]-nums[j])<=target  and dp[j]!=-1)
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
             }
+            
         }
-        return dp[i]=a;
-    }
-
-    int maximumJumps(vector<int>& nums, int target) {
-        dp.resize(nums.size()+1,-1);
-        int ans=func(nums,target,0);
-        return ans<0?-1:ans;
+        
+        return dp[n-1]<0?-1:dp[n-1];
     }
 };
+
+
+//         
+        // int ans=func(nums,tar:
+//     int func(vector<int>&nums,int target,int i)
+//     {
+//         if(i==nums.size()-1)return 0;
+
+//         if(dp[i]!=-1)return dp[i];
+//         int a=INT_MIN;
+//         for(int j=i+1;j<nums.size();j++)
+//         {
+            
+//         }
+//         return dp[i]=a;
+//     }
