e3af06fd147e13ec8373439e52a9e21d9b55d8dc
Tausif <91016348+tausif39@users.noreply.github.com>
2023-09-15 22:26:16 +0530

    Time: 115 ms (81.65%), Space: 107.3 MB (65.40%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -3,36 +3,60 @@ public:
     
     long long continuousSubarrays(vector<int>& nums) 
     {
-        
-        multiset<int> mp;
-        
-        int j=0,i=0;
-        
+        map<int,int> mp;
+        int i=0,j=0,n=nums.size();
         long long ans=0;
-        
-        while(j<nums.size())
+     
+        while(j<n)
         {
             
-            mp.insert(nums[j]);
+            int lo=0,hi=0;
             
-            while((*mp.rbegin()-*mp.begin())>2)
+            while(j<n )
+            {
+                mp[nums[j]]++;
+                auto it1=mp.begin(),it2=mp.end();
+                it2--;
+                lo=it1->first,hi=it2->first;
+               
+                if(hi-lo<=2)
+                {
+                    int temp=j-i+1;
+
+                    ans+=temp;
+                    j++;
+                }
+                else break;
+            }
+    
+            while(i<j and hi-lo>2)
             {
                 
-                mp.erase(mp.find(nums[i]));
+                mp[nums[i]]--;
+         
+                if(mp[nums[i]]==0)
+                {
+                    
+                    mp.erase(nums[i]);
+                }
+                
+                auto it1=mp.begin(),it2=mp.end();
+                it2--;
+                lo=it1->first,hi=it2->first;
                 i++;
                 
-            }    
-            
-            ans+=j-i+1;
-            
-            
-            j++;
+            }
+      
+            if(j<n)
+            {
+                int temp=j-i+1;
 
+                ans+=temp;
+            }
+          
+            j++;
         }
         
-        
         return ans;
-        
-
     }
 };
