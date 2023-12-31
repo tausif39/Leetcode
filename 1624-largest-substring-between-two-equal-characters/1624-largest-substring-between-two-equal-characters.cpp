8950a5a13b0e3e443d2c8ec5db23f8202c979d8d
Tausif <91016348+tausif39@users.noreply.github.com>
2023-12-31 11:57:15 +0530

    Time: 10 ms (5.74%), Space: 6.7 MB (75.31%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    int maxLengthBetweenEqualCharacters(string s) 
-    {
-        unordered_map<char,int> mp;
-        
-        
-        for(int i=0;i<s.size();i++)
+    int maxLengthBetweenEqualCharacters(string s) {
+        int count = -1;
+        int n = s.size() ;
+        for(int i= 0; i < n; i++)
         {
-           ++mp[s[i]];
-        }
-        
-        // vector<int> v;
-        int distance=0;
-        int cnt=0;
-        for(int i=0;i<s.size()-1;i++)
-        {
-           if(mp[s[i]]>=2)
-           {
-                cnt++;
-               
-                for(int j=i+1;j<s.size();j++)
+            for(int j = i+1; j < n; j++)
+            {
+                if(s[i] == s[j])
                 {
-                   if(s[i]==s[j])
-                   {
-                       distance=max(distance,abs(j-i));
-                       
-                   }
+                    count = max(count, j-i-1);
                 }
-               
-               
-           }
-            
+            }
         }
-        
-        
-        if(cnt==0) return -1;
-        
-        return (distance-1);
-        
+        return count ;
     }
 };
